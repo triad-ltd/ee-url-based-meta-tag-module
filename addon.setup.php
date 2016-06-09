73ca3f487f1d5cb24b658bd9bc13a5c8ac686aa5
Stephen Sweetland <stephen.sweetland@triad.uk.com>
2016-06-09 16:45:56 +0100

    Underway to make urlmetas work on ee3

---
+++ addon.setup.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * URL Metas
+ *
+ * @package             Url metas for EE3
+ * @author              Stephen Sweetland <stephen@triad.uk.com>
+ * @copyright           Copyright (c) 2016 Triad Ltd
+ * @link                http://triad.uk.com
+ */
+
+return array(
+	'author'      => 'Triad Ltd',
+	'author_url'  => 'http://triad.uk.com',
+	'name'        => 'URL Metas',
+	'description' => 'Lets you set url-specific meta information. FTW.',
+	'version'     => '2.0',
+	'namespace'   => 'User\Addons\UrlMetas'
+);
